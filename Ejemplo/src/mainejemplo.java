aa8b0c86cb2ee7d2784d1c6c40050f3c22ca1608
Usuario <Usuario@192.168.0.11>
2018-04-27 00:00:08 -0300

    Ejemplo TP1 parte 1

---
+++ Ejemplo/src/mainejemplo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class mainejemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre= JOptionPane.showInputDialog("Ingrese su nombre: ");
+    	JOptionPane.showMessageDialog(null, "Hola " + nombre);
+
+		
+	}
+	
+	}
