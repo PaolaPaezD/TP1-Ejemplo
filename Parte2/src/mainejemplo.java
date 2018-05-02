0ae6425493fcf6c98ef249f438149d98eb132eaf
alumno <alumno@192.168.0.10>
2018-05-01 23:48:10 -0300

    Parte 2 finalizada

---
+++ Parte2/src/mainejemplo.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class mainejemplo {
+public static void main(String[] args) {
+	
+	String nombre= JOptionPane.showInputDialog("Ingrese su nombre: ");
+	JOptionPane.showMessageDialog(null, "Hola " + nombre);
+	String numero=  JOptionPane.showInputDialog("Ingrese un numero entero: ");
+	JOptionPane.showMessageDialog(null, "Usted ingreso " + numero);
+
+			
+	}
+
+}
